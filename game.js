5d1088bc974e3b081e53d82634a79fbb647ad389
Nic Wilson <88619060+nicforzen@users.noreply.github.com>
2021-09-18 17:44:58 -0700

    Push

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 async function appEntry(canvas, localStorage) {
     let driver = new whm.Driver(new whm.Instance(new TestScene()), canvas, localStorage)
     driver.gameWidth = 100; driver.gameHeight = 100;
@@ -26,40 +27,55 @@ TestScene.prototype = new whm.Scene;
 
 function TestWall() {
     let ch = new whm.GameObject();
-    ch.renderer = new whm.RectangleRenderer(1, 1, whm.Color.RED);
-
     let s = new whm.Script();
     s.onMouseDown = function(e) {
         whm.Debug.log("Click! " + e.x + " " + e.y);
         // this.gameObject.transform.position = new whm.Vector2(e.x/10, e.y/10);
         // this.gameObject.rigidbody.velocity = new whm.Vector2();
         // this.gameObject.rigidbody._b2Body.setAwake(true);
-        let ch2 = new whm.GameObject();
-        let ss = new whm.Script();
-        ss.update = function() {
-            //console.log(this.gameObject.transform.rotation.radians);
-        }
-        ch2.addScript(ss);
-        ch2.transform.position = new whm.Vector2(e.x/10, e.y/10);
-        // ch2.transform.position = new whm.Vector2(e.x, e.y);
-        //ch2.transform.rotation.radians = -1.5708;
-        ch2.transform.rotation.radians = 1.5708;
-        ch2.renderer = new whm.RectangleRenderer(1, 1, whm.Color.RED);
-        this.gameObject.instance.addObject(ch2);
+        this.gameObject.instance.addObject(getBlock(e.x, e.y));
     }
-    s.onMouseScroll = function() {
+    s.onMouseScroll = function(e) {
         whm.Debug.log("SCROLL");
-        // TODO scroll camera
+        this.gameObject.instance.camera.setScale(
+            this.gameObject.instance.camera.scale + -0.10 * e);
     };
     s.update = function() {
-        //console.log(this.gameObject.transform.position.y);
-        //console.log(this.gameObject.rigidbody.velocity.y);
-        // console.log(this.gameObject.transform.rotation.radians);
+        // TODO DESTROY AFTER 10 seconds
     }
     s.start = function(){
-        whm.Debug.log("hi");
+        whm.Debug.log("Controller object loaded");
     }
-    ch.addScript(s);
+    ch.addComponent(s);
 
     return ch;
+}
+
+
+// MAKE THE FLOOR A GAMEOBJECT
+
+function getBlock(x, y){
+    let bx = Math.random() + 0.2;
+    let by = Math.random() + 0.2;
+    let colorInt = Math.floor(Math.random() * 4);
+    let color = whm.Color.ORANGE;
+    if(colorInt == 1) color = whm.Color.RED;
+    else if(colorInt == 2) color = whm.Color.BLUE;
+    else if(colorInt == 3) color = whm.Color.GREEN;
+    let g = new whm.GameObject();
+    g.transform.position = new whm.Vector2(x, y);
+    g.addComponent(new whm.Rigidbody());
+    let c = new whm.BoxCollider(bx, by);
+    c.friction = 0.3;
+    c.bounce = 0.3;
+    g.addComponent(c);
+    g.renderer = new whm.RectangleRenderer(bx, by, color);
+    let s = new whm.Script();
+    g.metadata.timer = 0;
+    s.update = function() {
+        this.gameObject.metadata.timer += this.gameObject.instance.deltaTime;
+        if(this.gameObject.metadata.timer > 10) this.gameObject.instance.destroyObject(this.gameObject);
+    }
+    g.addComponent(s);
+    return g;
 }
